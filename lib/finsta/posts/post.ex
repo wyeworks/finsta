6fc4557f9191b12f98d8972368168d5ca7d39a75
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2023-11-15 19:28:27 -0300

    likes table migration + schema

--- lib/finsta/posts/post.ex
+++ lib/finsta/posts/post.ex
@@ -6,6 +6,7 @@ defmodule Finsta.Posts.Post do
     field :caption, :string
     field :image_url, :string
     belongs_to :user, Finsta.Accounts.User
+    has_many :likes, Finsta.Posts.Like
 
     timestamps(type: :utc_datetime)
   end
---
+++ priv/repo/migrations/20231115204622_add_user_likes_table.exs
@@ -0,0 +1,13 @@
+defmodule Finsta.Repo.Migrations.AddUserLikesTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:likes) do
+      add :user_id, references(:users, on_delete: :nothing)
+      add :post_id, references(:posts, on_delete: :nothing)
+      timestamps(type: :utc_datetime)
+    end
+
+    create unique_index(:likes, [:user_id, :post_id])
+  end
+end
