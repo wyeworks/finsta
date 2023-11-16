8fee83584af9c7fd7a4bd15343930d94b2f7a158
Nicolás Cirio <nicolas.cirio@wyeworks.com>
2023-11-16 12:00:58 -0300

    delete all associated likes when deleting a post

--- priv/repo/migrations/20231115204622_add_user_likes_table.exs
+++ priv/repo/migrations/20231115204622_add_user_likes_table.exs
@@ -4,7 +4,7 @@ defmodule Finsta.Repo.Migrations.AddUserLikesTable do
   def change do
     create table(:likes) do
       add :user_id, references(:users, on_delete: :nothing)
-      add :post_id, references(:posts, on_delete: :nothing)
+      add :post_id, references(:posts, on_delete: :delete_all)
       timestamps(type: :utc_datetime)
     end
 
